c8a1a53a073a0bc18f307f717865deddff0aa943
Wairioko <charlesmungai5@gmail.com>
2025-02-05 06:36:52 +0300

    dashboard page service

---
+++ src/dashboard/service/dashboardService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getInvoices = async () => {
+    try {
+        const response = await axios.get(`${process.env.REACT_APP_AWS_URL}/api/dashboard/invoices`);
+        return response.data;
+    } catch (error) {
+        console.error("Failed to fetch invoices:", error);
+        throw error;
+    }
+};
