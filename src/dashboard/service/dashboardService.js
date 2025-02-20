0b0afed20a3cd8808390e654482c77a9e4d68f93
Wairioko <charlesmungai5@gmail.com>
2025-02-20 18:56:02 +0300

    service to get dahboard data

--- src/dashboard/service/dashboardService.js
+++ src/dashboard/service/dashboardService.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 export const getInvoices = async () => {
     try {
-        const response = await axios.get(`${process.env.REACT_APP_AWS_URL}/api/dashboard/invoices`);
+        const response = await axios.get(`${process.env.REACT_APP_AWS_URL}/api/dashboard`,
+            {
+            withCredentials: true,
+            withXSRFToken: true
+        });
         return response.data;
     } catch (error) {
         console.error("Failed to fetch invoices:", error);
