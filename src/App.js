f46d0ca60eb71e8b4a6cfcb85be8ba0a35ba18e9
Wairioko <charlesmungai5@gmail.com>
2025-03-18 11:07:54 +0300

    Addition of authprovider,checkedinvoices,dashboard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import HomePage from './home.js';
-import { Routes, Route } from'react-router-dom';
+import HomePage from './home.mjs';
 import LoginPage from './users/page/loginPage.mjs';
 import SignUpPage from './users/page/signupPage.mjs';
+import { AuthProvider } from './authprovider.mjs';
+import CheckedInvoices from './invoice/page/invoiceCheckerPage.mjs';
+import Dashboard from './dashboard/page/dashboard.mjs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
+  
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />   
+          <Route path='/invoices/status' element={<CheckedInvoices />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          
+        </Routes>
+      </AuthProvider>
    
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage /> } />
-        
-      </Routes>
-    
   );
 }
 
